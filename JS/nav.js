3561d1bf0b00144e11981688f8f52370ddba3a21
Micah Chollar <widgetilities@gmail.com>
2022-02-01 20:32:58 -0700

    add screenshots to apps, fixed menu for mobile

--- JS/nav.js
+++ JS/nav.js
@@ -6,3 +6,13 @@ fetch("nav.html")
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
   });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleMenu() {
+  var x = document.getElementById("myNavBar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
